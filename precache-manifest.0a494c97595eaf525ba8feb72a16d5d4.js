9c8467123b2e5953cc8e8b94492fea6feb7c161c
Romain Castillon <castillonromain01@gmail.com>
2018-11-14 20:58:56 +0100

    Updates

---
+++ precache-manifest.0a494c97595eaf525ba8feb72a16d5d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firebasewd/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "feb4cba72c5b7df15ba9",
+    "url": "/firebasewd/static/js/runtime~main.feb4cba7.js"
+  },
+  {
+    "revision": "b9fee1be6d995c84c5cd",
+    "url": "/firebasewd/static/js/main.b9fee1be.chunk.js"
+  },
+  {
+    "revision": "fa60b6a11549709aa97c",
+    "url": "/firebasewd/static/js/1.fa60b6a1.chunk.js"
+  },
+  {
+    "revision": "b9fee1be6d995c84c5cd",
+    "url": "/firebasewd/static/css/main.a39c242c.chunk.css"
+  },
+  {
+    "revision": "d4d161a251f105b73c97f8b89e7cc6b5",
+    "url": "/firebasewd/index.html"
+  }
+];
